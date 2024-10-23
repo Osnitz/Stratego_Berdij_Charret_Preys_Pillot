d851ef6d79debc37d6bf00c416527aa8dd83f581
Souhaila-Berdij <souhaila.berdij@ensea.fr>
2024-10-23 11:00:03 +0200

    board

--- src/shared/state/main.cpp
+++ src/shared/state/main.cpp
@@ -2,14 +2,16 @@
 // Created by matthieu on 19/10/24.
 //
 #include <iostream>
-#include "Game.h"
+//#include "Game.h"
+#include "Board.h"
 
 using namespace std;
 using namespace state;
 int main() {
-    Game game;
-
-    game.startGame();
+    //Game game;
+    Board *board = Board::getInstance();
+    board->displayBoard();
+    //game.startGame();
 
     return 0;
 }
