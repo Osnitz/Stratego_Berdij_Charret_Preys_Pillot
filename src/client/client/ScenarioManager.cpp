6a07c6647814ad3f4629920b8bf51b8f1e5567d2
Osnitz <lolmatt18@gmail.com>
2024-12-11 11:43:02 +0100

    modification ScenarioManager.cpp

--- src/client/client/ScenarioManager.cpp
+++ src/client/client/ScenarioManager.cpp
@@ -2,15 +2,8 @@
 #define SCENARIOMANAGER_H
 
 #include <memory>
-#include "engine.h"
 #include "client.h"
 #include "ai.h"
-#include <utility>
-
-template<class T, class... Args>
-std::unique_ptr<T> make_unique(Args&&... args) {
-    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
-}
 
 enum class GameMode {
     PVP,   // Joueur vs Joueur
